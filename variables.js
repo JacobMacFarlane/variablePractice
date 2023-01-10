790bc35796a37c329735ca8c7b2d8902d0c53625
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-01-10 12:15:23 -0700

    Show the final Product

--- variables.js
+++ variables.js
@@ -2,6 +2,12 @@
 "The cow is walking"
 "Deftones rock"
 var startingAge = 19
+startingAge = 20
 var golfScore = 98
 var depositPaid = true
+depositPaid = false
 var superCool = false
+console.log(startingAge)
+console.log(depositPaid)
+console.log(golfScore)
+console.log(superCool)
