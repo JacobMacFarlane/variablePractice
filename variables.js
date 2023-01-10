85155f17c29d8d50e2ad761fc953774af51fb721
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-01-10 12:18:28 -0700

    Show the complete final

--- variables.js
+++ variables.js
@@ -1,6 +1,6 @@
-"jjmacfarlane943@gmail.com"
-"The cow is walking"
-"Deftones rock"
+var userName = "jjmacfarlane943@gmail.com"
+var phrase = "The cow is walking"
+var musicArtist = "Deftones rock"
 var startingAge = 19
 startingAge = 20
 var golfScore = 98
@@ -11,3 +11,5 @@ console.log(startingAge)
 console.log(depositPaid)
 console.log(golfScore)
 console.log(superCool)
+console.log(userName)
+console.log(phrase)
